89c5c50e8e5d71f485dbe9a442a6c6a1108751f2
Patrick Stapfer <p.stapfer@gmail.com>
2012-03-14 11:54:36 +0100

    Initial Commit for Project-Repository

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Patrick Stapfer
+ * Date: 14.03.12
+ * Time: 11:46
+ */
+public class Main{
+    public static void Main(String ... args){
+        //TODO: GUI starten
+    }
+}
