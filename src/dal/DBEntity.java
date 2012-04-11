050119fe04f40821ec7f988d0dbca7eecb00e58a
Patrick Stapfer <p.stapfer@gmail.com>
2012-04-11 14:17:11 +0200

    Methode getID() in der DBEntity-Schnittstelle hinzugefügt, damit der PK-Value ausgelesen werden kann

--- src/dal/DBEntity.java
+++ src/dal/DBEntity.java
@@ -1,6 +1,6 @@
 package dal;
 
-import java.sql.ResultSet;
+import java.lang.reflect.Field;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,8 +17,37 @@ public abstract class DBEntity {
 
     }
 
-    public DBEntity(ResultSet rs){
+    /**
+     * Returns the Primary-Key-Value of the DBEntity-object
+     * This method must return the proper value of a get...ID() method
+     * For instance:
+     * public class Angebot{
+     *     private int angebotID;
+     *
+     *     public int getAngebotID(){
+     *         return angebotID;
+     *     }
+     *
+     *     public Object getID(){
+     *         return getAngebotID();
+     *     }
+     * }
+     * @return Value of the primary-key-field
+     */
+    public abstract Object getID();
 
+    /**
+     * Returns the Reflection-Field of a DBEntity-class-definition, which represents the Primary-Key in its database
+     * @param entityClass - DBEntity-Class for retrieving the PK field
+     * @return Field-Definition of the PK-Field or NULL if it could not be retrieved
+     */
+    public static Field getPKField(Class<? extends DBEntity> entityClass){
+        String table = entityClass.getSimpleName();
+
+        for(Field f : entityClass.getFields()){
+            if(f.getType().equals(f.getName().equals(table.toLowerCase()+"ID")))
+                return f;
+        }
+        return null;
     }
-
 }
