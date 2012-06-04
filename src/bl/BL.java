e3549919bdd86e4097878cb21d6141107883a204
Armin Mehr <armin_juju@hotmail.com>
2012-06-04 18:09:10 +0200

    Konfiguration und Logging-Modul im Code eingebaut

--- src/bl/BL.java
+++ src/bl/BL.java
@@ -15,11 +15,16 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import logging.LoggerManager;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import config.ConfigException;
+import config.Configuration;
 
 import bl.objects.Angebot;
 import bl.objects.Ausgangsrechnung;
@@ -73,8 +78,19 @@ public class BL {
 	private static int kategorieID = 0;
 	private static ArrayList<Rechnung_Buchungszeile> rechnungen_buchungszeilen = new ArrayList<Rechnung_Buchungszeile>();
 
-	private static DatabaseAdapter db = new MysqlAdapter("root", "dbsy",
-			"localhost", "swe");
+
+	private static DatabaseAdapter db = null;
+	// private static DatabaseAdapter db = new MysqlAdapter("root", "dbsy",
+	// "localhost", "swe");
+
+	static {
+		try {
+			db = Configuration.getInstance().getDatabaseAdapter();
+		} catch (ConfigException e) {
+			LoggerManager.getLogger("runtime").warn("Konfiguration fehlerhaft");
+			System.exit(1);
+		}
+	}
 
 	public BL() {
 		projektliste = new ArrayList<Projekt>();
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -11,27 +11,35 @@ import logging.FileAdapter;
 import logging.Logger;
 import logging.LoggerManager;
 import logging.LoggingLevel;
+import config.ConfigException;
+import config.Configuration;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
+		Configuration config = null;
+		try {
+			config = Configuration.getInstance();
+		} catch (ConfigException e2) {
+			e2.printStackTrace();
+		}
+
 		Logger l = new Logger();
 		try {
-			l.addAdapter(new FileAdapter(new File("log"), "xmlImport"));
-			//TODO: auf config ueberpruefen
-			l.addAdapter(new ConsoleAdapter());
-			
-			LoggerManager.registerLogger("xmlImport", l);
-			
-			//TODO: auf config ueberpruefen
-			LoggerManager.setLoggingLevel(LoggingLevel.DEBUG);
+			l.addAdapter(new FileAdapter(config.getLoggingDirectory(),
+					"runtime"));
+
+			if (config.isLoggingStdout()) {
+				l.addAdapter(new ConsoleAdapter());
+			}
+
+			LoggerManager.registerLogger("runtime", l);
+
+			LoggerManager.setLoggingLevel(config.getLoggingLevel());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-		
+
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException e1) {
@@ -43,23 +51,23 @@ public class Main {
 		} catch (UnsupportedLookAndFeelException e1) {
 			e1.printStackTrace();
 		}
-//		Toolkit.getDefaultToolkit().getSystemEventQueue()
-//				.push(new EventQueue() {
-//					@Override
-//					protected void dispatchEvent(AWTEvent event) {
-//						if (event instanceof KeyEvent) {
-//							KeyEvent keyEvent = (KeyEvent) event;
-//							if (KeyEvent.KEY_RELEASED == keyEvent.getID()
-//									&& KeyEvent.VK_ESCAPE == keyEvent
-//											.getKeyCode()
-//									&& keyEvent.getSource() instanceof JDialog) {
-//								JDialog d = ((JDialog) keyEvent.getSource());
-//									d.dispose();
-//							}
-//						}
-//						super.dispatchEvent(event);
-//					}
-//				});
+		// Toolkit.getDefaultToolkit().getSystemEventQueue()
+		// .push(new EventQueue() {
+		// @Override
+		// protected void dispatchEvent(AWTEvent event) {
+		// if (event instanceof KeyEvent) {
+		// KeyEvent keyEvent = (KeyEvent) event;
+		// if (KeyEvent.KEY_RELEASED == keyEvent.getID()
+		// && KeyEvent.VK_ESCAPE == keyEvent
+		// .getKeyCode()
+		// && keyEvent.getSource() instanceof JDialog) {
+		// JDialog d = ((JDialog) keyEvent.getSource());
+		// d.dispose();
+		// }
+		// }
+		// super.dispatchEvent(event);
+		// }
+		// });
 		new HauptFrame();
 	}
 }
